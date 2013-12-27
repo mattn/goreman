ef73d8d9a8746839b9e0b9f4f059e63d87f4787e
mattn <mattn.jp@gmail.com>
2013-12-27 13:15:39 +0900

    Add go

---
+++ eg/web.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "flag"
+    "github.com/hoisie/web"
+)
+
+var addr = flag.String("a", ":8080", "address")
+
+func main() {
+	flag.Parse()
+	web.Get("/", func() string {
+		return "Hello World"
+	})
+	web.Run(":8080")
+}
