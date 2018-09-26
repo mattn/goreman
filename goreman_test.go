c83eaf2069d1c445ff56aa8b446bd98e491ffe04
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-09-26 23:46:41 +0900

    cosmetic change

--- goreman_test.go
+++ goreman_test.go
@@ -18,29 +18,28 @@ func TestMain(m *testing.M) {
 	var err error
 	sleep, err = exec.LookPath("sleep")
 	if err != nil {
-		if runtime.GOOS == "windows" {
-			code := `package main;import ("os";"strconv";"time");func main(){i,_:=strconv.ParseFloat(os.Args[1]);time.Sleep(time.Duration(i)*time.Second)}`
-			dir, err := ioutil.TempDir("", "goreman-test")
-			if err != nil {
-				panic(err)
-			}
-			sleep = filepath.Join(dir, "sleep.exe")
-			src := filepath.Join(dir, "sleep.go")
-			err = ioutil.WriteFile(src, []byte(code), 0644)
-			if err != nil {
-				panic(err)
-			}
-			b, err := exec.Command("go", "build", "-o", sleep, src).CombinedOutput()
-			if err != nil {
-				panic(string(b))
-			}
-		} else {
+		if runtime.GOOS != "windows" {
 			panic(err)
+		}
+
+		code := `package main;import ("os";"strconv";"time");func main(){i,_:=strconv.ParseFloat(os.Args[1]);time.Sleep(time.Duration(i)*time.Second)}`
+		dir, err := ioutil.TempDir("", "goreman-test")
+		if err != nil {
+			panic(err)
+		}
+		sleep = filepath.Join(dir, "sleep.exe")
+		src := filepath.Join(dir, "sleep.go")
+		err = ioutil.WriteFile(src, []byte(code), 0644)
+		if err != nil {
+			panic(err)
+		}
+		b, err := exec.Command("go", "build", "-o", sleep, src).CombinedOutput()
+		if err != nil {
+			panic(string(b))
 		}
 		oldpath := os.Getenv("PATH")
 		os.Setenv("PATH", dir+";"+oldpath)
 		defer os.Setenv("PATH", oldpath)
-
 	}
 	r := m.Run()
 
